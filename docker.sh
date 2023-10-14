5849912d347aead1f5c4d0c164b8757e740b9867
john <1525628670@qq.com>
2023-10-14 22:10:45 +0800

    added docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/root/NyuziProcessor -w /root/NyuziProcessor jeffbush001/nyuzi-build /bin/bash -c scripts/run_ci_tests.sh
+sudo docker run -v $(pwd):/root/NyuziProcessor -w /root/NyuziProcessor jeffbush001/nyuzi-build /bin/bash -c scripts/run_ci_tests.sh
